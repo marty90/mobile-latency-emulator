968a991f572f362638dce1f819e9a9f86b03637d
martino <martino.trevisan@studenti.polito.it>
2019-07-01 14:50:51 +0200

    check script arguments

--- start_shaping.sh
+++ start_shaping.sh
@@ -4,6 +4,29 @@
 IFACE=$1
 PROFILE=$2
 
+# Check number of arguments
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters."
+    echo "Usage: ./start_shaping.sh [INTERFACE] [PROFILE]"
+    exit 1
+fi
+
+# Check interface exists
+found=$( ifconfig  | grep Link | cut -d " " -f 1 | grep $IFACE)
+if [ -z $found ]
+then
+      echo "Error: interface $IFACE does not exist. Quitting..."
+      exit 1
+fi
+
+# Check profile exists
+found=$( ls profiles  | awk "\$1 == \"$PROFILE\" ")
+if [ -z $found ]
+then
+      echo "Error: profile $PROFILE does not exist. Quitting..."
+      exit 1
+fi
+
 # Get Profile Information
 infos=$(cat profile_info.txt | awk "\$1==\"$PROFILE\"")
 mu=$( echo $infos | cut -d " " -f 2)
--- stop_shaping.sh
+++ stop_shaping.sh
@@ -3,5 +3,20 @@
 # Parse args
 IFACE=$1
 
+# Check number of arguments
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters."
+    echo "Usage: ./stop_shaping.sh [INTERFACE]"
+    exit 1
+fi
+
+# Check interface exists
+found=$( ifconfig  | grep Link | cut -d " " -f 1 | grep $IFACE)
+if [ -z $found ]
+then
+      echo "Error: interface $IFACE does not exist. Quitting..."
+      exit 1
+fi
+
 #Stop Shaping
 tc qdisc del dev $IFACE root
